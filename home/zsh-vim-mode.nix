2a967d27efcbac2aa736f688cd3d6440476060a9
Obi Shawn Kenobi <obishawnkenobi@pm.me>
2025-02-10 15:01:18 -0600

    Fix: Update zsh configs

--- home/zsh-vim-mode.nix
+++ home/zsh-vim-mode.nix
@@ -17,12 +17,12 @@
 
     # Change cursor shape for different vi modes
     function zle-keymap-select {
-      if [[ ${KEYMAP} == vicmd ]] ||
+      if [[ ''${KEYMAP} == vicmd ]] ||
          [[ $1 = 'block' ]]; then
         echo -ne '\e[1 q'
-      elif [[ ${KEYMAP} == main ]] ||
-           [[ ${KEYMAP} == viins ]] ||
-           [[ ${KEYMAP} = "" ]] ||
+      elif [[ ''${KEYMAP} == main ]] ||
+           [[ ''${KEYMAP} == viins ]] ||
+           [[ ''${KEYMAP} = "" ]] ||
            [[ $1 = 'beam' ]]; then
         echo -ne '\e[5 q'
       fi
